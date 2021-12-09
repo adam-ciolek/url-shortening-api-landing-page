88ea1f1fbf80afaf841fddb5ed8b09ab355ad48a
Adam Ciołek <adasciolek@gmail.com>
2021-12-09 12:01:28 +0100

    url-page

---
+++ app.js
@@ -0,0 +1,56 @@
+
+const btn = document.querySelector('.btn-submit');
+const linkShorten = document.querySelector('.shorten-link');
+
+btn.addEventListener('click', () => {
+    const input = document.querySelector('.input').value;
+    
+    const url = `https://api.shrtco.de/v2/shorten?url=${input}`
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => show(data , input))
+      
+})
+    
+const show = (data , input) => {
+
+    if(input === '') return;
+    console.log(data)
+    const div = document.createElement('div');
+    
+    div.innerHTML = 
+    `
+        <div class="display-f justify-space-between bg-white a mb-2">
+            <div>
+                <p class="font-lg">${input}</p>
+            </div>
+            <div class="display-f align-item-center">
+                <p class="font-lg text-primary mr-4 link" id="${data.result.code}">${data.result.full_short_link}</p>
+                <button class="b" onclick="copyLink(this.getAttribute('data-target'))" data-target ="${data.result.code}">Copy</button>
+            </div>
+        </div>
+    `
+    linkShorten.appendChild(div)  
+
+}
+
+// Copy link
+
+const copyLink = target => {
+    console.log(target)
+
+   var copy = document.getElementById(target);
+
+    const btn = document.querySelector('.b')
+
+   navigator.clipboard.writeText(copy.innerHTML);
+
+    
+    if(target === btn['dataset'].target) {
+        console.log('true')
+        btn.innerHTML = 'Copied';
+        btn.style.backgroundColor = 'hsl(255, 11%, 22%)';
+    }
+
+}
